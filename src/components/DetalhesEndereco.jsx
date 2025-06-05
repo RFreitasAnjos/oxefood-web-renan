150bbca8efd86733188d7415da207947eca5fb1b
Renan Freitas <renan.freitas.anjos@gmail.com>
2025-06-04 23:52:48 -0300

    fix: show details

--- src/components/DetalhesEndereco.jsx
+++ src/components/DetalhesEndereco.jsx
@@ -1,24 +1,42 @@
+import { useEffect, useState } from "react";
 import { Button, Icon } from "semantic-ui-react";
-import { removerEndereco } from '../Controller/cliente/ControllerCliente.jsx';
+import { removerEndereco } from "../Controller/cliente/ControllerCliente";
 
 export const DetalhesEndereco = ({ enderecos }) => {
+  const[lista, setLista] = useState([]);
+      
+  useEffect(() => {
+      setLista(enderecos);
+    }, [enderecos]);
+
+  const remover = async (id) => {
+    try{
+      await removerEndereco(id);
+      const novaLista = lista.filter((end) => end.id !== id);
+      setLista(novaLista);
+    } catch ( error ) {
+      alert("Erro ao carregar lista");
+      console.error("Erro ao carregar lista:", error);
+    }
+  }
+
   return (
     <div>
       <h3>Endereços</h3>
       <hr/>
-      {enderecos.map((end, index) => (
+      {lista.map((end, index) => (
         <div key={index} style={{ marginBottom: '1em', flexDirection:'row'}}>
           <div>
             <p><strong>Rua:</strong> {end.rua}</p>
             <p><strong>Número:</strong> {end.numero}</p>
             <p><strong>Cidade:</strong> {end.cidade}</p>
             <p><strong>Estado:</strong> {end.estado}</p>
-          </div>
+          </div>  
           <div>
             <Button
               color="red"
               icon
-              onClick={() => removerEndereco(enderecos.id)}
+              onClick={() => remover(end.id)}
             >
               <Icon name="trash"/>
             </Button>
